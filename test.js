91a8969387e629535cb7fedf36e7ee0fb753237a
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2011-03-07 02:21:14 +0000

    ファイルをコピーするサンプルコードに置き換え。
    git-svn-id: https://madojs.googlecode.com/svn/trunk@11 76a504df-b32f-f248-8bcb-846a6bbce1e4

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-
+File.open("copy_of_mado.js", "w", function(outfile) {
+  File.open("mado.js", "r", function(infile) {
+    infile.each(function(line) {
+      outfile.puts(line);
+    });
+  });
+});

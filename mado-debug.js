8bacc3d67b3d18d605bba7f41e3c81acdf0b382e
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2011-04-12 05:23:11 +0000

    String#startsWith、String#endsWithメソッドを追加。
    git-svn-id: https://madojs.googlecode.com/svn/trunk@24 76a504df-b32f-f248-8bcb-846a6bbce1e4

--- mado-debug.js
+++ mado-debug.js
@@ -542,6 +542,31 @@ String.prototype.trim = function() {
   var text = this.valueOf();
   return (text || "").replace( /^\s+|\s+$/g, "" );
 };
+
+/**
+ * 文字列がchで始まるかどうか
+ * @param {String} ch テストする文字
+ * @return {Boolean} 文字列がchで始まる場合true、そうでない場合はfalseを返す。
+ * @example 使用例：
+"abc".startsWith("a"); // true
+ */
+String.prototype.startsWith = function(ch) {
+  var text = this.valueOf();
+  return(text.substring(0, 1) === ch);
+};
+
+/**
+ * 文字列がchで終わるかどうか
+ * @param {String} ch テストする文字
+ * @return {Boolean} 文字列がchで終わる場合true、そうでない場合はfalseを返す。
+ * @example 使用例：
+"abc".endsWith("c"); // true
+ */
+String.prototype.endsWith = function(ch) {
+  var text = this.valueOf();
+  var textLength = text.length;
+  return(text.substring(textLength - 1, textLength) === ch);
+};
 /** 
  * ファイルオブジェクトを作成します。
  * @class ファイルの操作と読み書きを行うためのクラスです。
--- test/string.js
+++ test/string.js
@@ -2,4 +2,20 @@ Test.String = {};
 
 Test.String.testTrim = function() {
   testUtil.assertEquals("@abc@", "@" + "   	abc			".trim() + "@");
-}
+};
+
+Test.String.testStartsWith = function() {
+  testUtil.assertEquals(true, "abc".startsWith("a"));
+};
+
+Test.String.testStartsWith2 = function() {
+  testUtil.assertEquals(false, "abc".startsWith("b"));
+};
+
+Test.String.testEndsWith = function() {
+  testUtil.assertEquals(true, "abc".endsWith("c"));
+};
+
+Test.String.testEndsWith = function() {
+  testUtil.assertEquals(false, "abc".endsWith("b"));
+};

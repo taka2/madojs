7d660ae4ae04e0d72c94574d92889402ebee2f86
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2011-04-22 03:09:10 +0000

    Clipboardのテストでcloseしていないために、 Excelのプロセスが残ってしまう不具合を修正。
    git-svn-id: https://madojs.googlecode.com/svn/trunk@34 76a504df-b32f-f248-8bcb-846a6bbce1e4

--- test/clipboard.js
+++ test/clipboard.js
@@ -4,10 +4,26 @@ Test.Clipboard.testHankaku = function() {
   var clip = new Clipboard();
   clip.set("abcde");
   testUtil.assertEquals("abcde", clip.get());
+  clip.close();
+};
+
+Test.Clipboard.testHankaku2 = function() {
+  new Clipboard.open(function(clip) {
+    clip.set("fghij");
+    testUtil.assertEquals("fghij", clip.get());
+  });
 };
 
 Test.Clipboard.testZenkaku = function() {
   var clip = new Clipboard();
   clip.set("‚ ‚¢‚¤‚¦‚¨");
   testUtil.assertEquals("‚ ‚¢‚¤‚¦‚¨", clip.get());
+  clip.close();
+};
+
+Test.Clipboard.testZenkaku2 = function() {
+  new Clipboard.open(function(clip) {
+    clip.set("‚©‚«‚­‚¯‚±");
+    testUtil.assertEquals("‚©‚«‚­‚¯‚±", clip.get());
+  });
 };

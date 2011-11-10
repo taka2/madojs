bf853ac75039cbff2f7076b1c17da52c80f7a866
takahiro.shigemoto <takahiro.shigemoto@gmail.com>
2011-11-10 11:41:43 +0000

    Excelクラスにカラーインデックスの定数を追加。
    git-svn-id: https://madojs.googlecode.com/svn/trunk@61 76a504df-b32f-f248-8bcb-846a6bbce1e4

--- mado-debug.js
+++ mado-debug.js
@@ -1840,7 +1840,6 @@ AdoConnection.prototype = {
       for(fe.moveFirst(); !fe.atEnd(); fe.moveNext()) {
         var field = fe.item();
         record[field.Name] = field.Value;
-        print(field.Name);
       }
       result.push(record);
 
@@ -2994,6 +2993,286 @@ Excel.COLUMN_TYPE_SKIP = 9;
  * カラムタイプ：xlEMDFormat(10)
  */
 Excel.COLUMN_TYPE_EMD = 10;
+
+/**
+ * カラーインデックス：1<span style = 'color: #000000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_01 = 1;
+
+/**
+ * カラーインデックス：2<span style = 'color: #FFFFFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_02 = 2;
+
+/**
+ * カラーインデックス：3<span style = 'color: #0000FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_03 = 3;
+
+/**
+ * カラーインデックス：4<span style = 'color: #00FF00'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_04 = 4;
+
+/**
+ * カラーインデックス：5<span style = 'color: #FF0000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_05 = 5;
+
+/**
+ * カラーインデックス：6<span style = 'color: #00FFFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_06 = 6;
+
+/**
+ * カラーインデックス：7<span style = 'color: #FF00FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_07 = 7;
+
+/**
+ * カラーインデックス：8<span style = 'color: #FFFF00'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_08 = 8;
+
+/**
+ * カラーインデックス：9<span style = 'color: #000080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_09 = 9;
+
+/**
+ * カラーインデックス：10<span style = 'color: #008000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_10 = 10;
+
+/**
+ * カラーインデックス：11<span style = 'color: #800000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_11 = 11;
+
+/**
+ * カラーインデックス：12<span style = 'color: #008080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_12 = 12;
+
+/**
+ * カラーインデックス：13<span style = 'color: #800080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_13 = 13;
+
+/**
+ * カラーインデックス：14<span style = 'color: #808000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_14 = 14;
+
+/**
+ * カラーインデックス：15<span style = 'color: #C0C0C0'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_15 = 15;
+
+/**
+ * カラーインデックス：16<span style = 'color: #808080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_16 = 16;
+
+/**
+ * カラーインデックス：17<span style = 'color: #FF9999'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_17 = 17;
+
+/**
+ * カラーインデックス：18<span style = 'color: #663399'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_18 = 18;
+
+/**
+ * カラーインデックス：19<span style = 'color: #CCFFFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_19 = 19;
+
+/**
+ * カラーインデックス：20<span style = 'color: #FFFFCC'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_20 = 20;
+
+/**
+ * カラーインデックス：21<span style = 'color: #660066'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_21 = 21;
+
+/**
+ * カラーインデックス：22<span style = 'color: #8080FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_22 = 22;
+
+/**
+ * カラーインデックス：23<span style = 'color: #CC6600'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_23 = 23;
+
+/**
+ * カラーインデックス：24<span style = 'color: #FFCCCC'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_24 = 24;
+
+/**
+ * カラーインデックス：25<span style = 'color: #800000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_25 = 25;
+
+/**
+ * カラーインデックス：26<span style = 'color: #FF00FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_26 = 26;
+
+/**
+ * カラーインデックス：27<span style = 'color: #00FFFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_27 = 27;
+
+/**
+ * カラーインデックス：28<span style = 'color: #FFFF00'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_28 = 28;
+
+/**
+ * カラーインデックス：29<span style = 'color: #800080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_29 = 29;
+
+/**
+ * カラーインデックス：30<span style = 'color: #000080'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_30 = 30;
+
+/**
+ * カラーインデックス：31<span style = 'color: #808000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_31 = 31;
+
+/**
+ * カラーインデックス：32<span style = 'color: #FF0000'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_32 = 32;
+
+/**
+ * カラーインデックス：33<span style = 'color: #FFCC00'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_33 = 33;
+
+/**
+ * カラーインデックス：34<span style = 'color: #FFFFCC'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_34 = 34;
+
+/**
+ * カラーインデックス：35<span style = 'color: #CCFFCC'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_35 = 35;
+
+/**
+ * カラーインデックス：36<span style = 'color: #99FFFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_36 = 36;
+
+/**
+ * カラーインデックス：37<span style = 'color: #FFCC99'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_37 = 37;
+
+/**
+ * カラーインデックス：38<span style = 'color: #CC99FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_38 = 38;
+
+/**
+ * カラーインデックス：39<span style = 'color: #FF99CC'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_39 = 39;
+
+/**
+ * カラーインデックス：40<span style = 'color: #99CCFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_40 = 40;
+
+/**
+ * カラーインデックス：41<span style = 'color: #FF6633'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_41 = 41;
+
+/**
+ * カラーインデックス：42<span style = 'color: #CCCC33'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_42 = 42;
+
+/**
+ * カラーインデックス：43<span style = 'color: #00CC99'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_43 = 43;
+
+/**
+ * カラーインデックス：44<span style = 'color: #00CCFF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_44 = 44;
+
+/**
+ * カラーインデックス：45<span style = 'color: #0099FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_45 = 45;
+
+/**
+ * カラーインデックス：46<span style = 'color: #0066FF'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_46 = 46;
+
+/**
+ * カラーインデックス：47<span style = 'color: #996666'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_47 = 47;
+
+/**
+ * カラーインデックス：48<span style = 'color: #969696'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_48 = 48;
+
+/**
+ * カラーインデックス：49<span style = 'color: #663300'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_49 = 49;
+
+/**
+ * カラーインデックス：50<span style = 'color: #669933'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_50 = 50;
+
+/**
+ * カラーインデックス：51<span style = 'color: #003300'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_51 = 51;
+
+/**
+ * カラーインデックス：52<span style = 'color: #003333'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_52 = 52;
+
+/**
+ * カラーインデックス：53<span style = 'color: #003399'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_53 = 53;
+
+/**
+ * カラーインデックス：54<span style = 'color: #663399'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_54 = 54;
+
+/**
+ * カラーインデックス：55<span style = 'color: #993333'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_55 = 55;
+
+/**
+ * カラーインデックス：56<span style = 'color: #333333'>■■■■■</span>
+ */
+Excel.COLOR_INDEX_56 = 56;
 
 /** 
  * Excelファイルを開き、ブロックを実行します。

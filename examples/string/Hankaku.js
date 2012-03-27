e424284856d2655206579f874bc63d32cc774cd2
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2012-03-27 18:31:12 +0900

    examples/string/hankakuを追加。

---
+++ examples/string/Hankaku.js
@@ -0,0 +1,11 @@
+if("±".isHankakuKana()) {
+  print("± is hankaku kana.");
+} else {
+  print("± is not hankaku kana.");
+}
+
+if("§".isHankakuKana()) {
+  print("§ is hankaku kana.");
+} else {
+  print("§ is not hankaku kana.");
+}

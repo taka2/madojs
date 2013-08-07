b9b57fdbfb3459d39da829171cb61f4e85bf9ffb
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2013-08-07 22:17:18 +0900

    Issue #23 Excelがインストールされていないと、ClipboardExcelのテストがfailする

--- test/clipboardexcel.js
+++ test/clipboardexcel.js
@@ -1,20 +1,26 @@
 TestCase("ClipboardExcel Test", {
   testOpen: function() {
-    ClipboardExcel.open(function(clip) {
-      clip.set("12345");
-      assertEquals("12345", clip.get());
-    });
+    if(Excel.available()) {
+      ClipboardExcel.open(function(clip) {
+        clip.set("12345");
+        assertEquals("12345", clip.get());
+      });
+    }
   },
   testHankaku: function() {
-    var clip = new ClipboardExcel();
-    clip.set("abcde");
-    assertEquals("abcde", clip.get());
-    clip.close();
+    if(Excel.available()) {
+      var clip = new ClipboardExcel();
+      clip.set("abcde");
+      assertEquals("abcde", clip.get());
+      clip.close();
+    }
   },
   testZenkaku: function() {
-    var clip = new ClipboardExcel();
-    clip.set("‚ ‚¢‚¤‚¦‚¨");
-    assertEquals("‚ ‚¢‚¤‚¦‚¨", clip.get());
-    clip.close();
+    if(Excel.available()) {
+      var clip = new ClipboardExcel();
+      clip.set("‚ ‚¢‚¤‚¦‚¨");
+      assertEquals("‚ ‚¢‚¤‚¦‚¨", clip.get());
+      clip.close();
+    }
   }
 });
